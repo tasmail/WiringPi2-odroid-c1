ef795f492c0fd804eb6d7d094f8912938730830d
Andrii Tkachenko <A.Tkachenko@foss.kharkov.ua>
2016-08-16 09:39:25 +0200

    Added build-python3.sh for python 3.x build. test.py updated.

---
+++ build-python3.sh
@@ -0,0 +1,3 @@
+swig3.0 -python wiringpi.i
+sudo python3 setup.py build install
+sudo python3 test.py
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import wiringpi2 as wiringpi
-io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
-print io.digitalRead(1)
-print io.analogRead(1)
+
+wiringpi.wiringPiSetup()
+
+print wiringpi.digitalRead(1)
+print wiringpi.analogRead(1)
